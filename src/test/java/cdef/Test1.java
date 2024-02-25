2f5636b769d1402a18bf58d624159a8d4a5b83eb
avinash giri <giriavinash5383@gmail.com>
2024-02-25 19:43:33 +0530

    added comment

--- src/test/java/cdef/Test1.java
+++ src/test/java/cdef/Test1.java
@@ -8,6 +8,7 @@ public class Test1 {
 	
 
 public Properties readprop () throws IOException {
+	//read the configuration from properties file
 	FileInputStream fis= new FileInputStream("src/test/resources/configtest.properties");
 	Properties prop = new Properties();
 	prop.load(fis);
